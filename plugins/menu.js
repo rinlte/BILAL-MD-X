e21d8fdb5710e36438bb6654db51eba795a5d8ad
BILAL TECH <penalacc786@gmail.com>
2025-10-09 06:27:47 +0500

    👑 BILAL-MD 👑

--- plugins/menu.js
+++ plugins/menu.js
@@ -24,6 +24,7 @@ cmd({
 *┃👑│ MODE :❯ ${config.MODE}*
 *┃👑│ PREFiX :❯ ${config.PREFIX}*
 *┃👑│ UPTIME :❯ ${runtime(process.uptime())}*
+*┃👑│ PLATFORM :❯ ${getPlatform()}*
 *┃👑╰──────────────*
 *╰━━━━━━━━━━━━━━━┈⊷*
 
