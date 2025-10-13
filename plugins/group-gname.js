97d2c58cad856842141f5bfce6668824490e7643
BILAL TECH <penalacc786@gmail.com>
2025-10-13 21:22:03 +0500

    👑 BILAL-MD 👑

--- plugins/group-gname.js
+++ plugins/group-gname.js
@@ -31,7 +31,7 @@ async (conn, mek, m, { from, isGroup, isAdmins, isBotAdmins, args, q, reply }) =
         await conn.groupUpdateSubject(from, q);
 
         // Reply with updated name
-        const updatedMsg = `*GROUP KA NAME CHANGE HO CHUKA HAI 🥺* \n *AB YEH 👇 NAME HAI GROUP KA OK ☺️ AGAR KISI KO KOI MASLA HO IS NAME SE 😒 TO ABHI BATA DE MUJHE 🥺♥️*\n\n${q}`;
+        const updatedMsg = `*GROUP KA NAME CHANGE HO CHUKA HAI 🥺* \n *AGAR KISI KO KOI MASLA HO  😒 TO ABHI BATA DE MUJHE 🥺 \n *MENE YEH NAME RAKHAA HAI 👇 GROUP KA*\n\n${q}`;
         await reply(updatedMsg);
 
         // React command message after successful update
