b6de050f059edd4d7ac9aff0c1476e2fafada075
BILAL TECH <penalacc786@gmail.com>
2025-10-13 14:18:45 +0500

    👑 BILAL-MD 👑

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -11,11 +11,11 @@ cmd({
   alias: ["status", "online", "a", "active"],
   desc: "Check bot is alive or not",
   category: "main",
-  react: "😊",
+  react: "🥰",
   filename: __filename
 }, async (conn, mek, m, { from, sender, reply }) => {
   try {
-    const status = `*ASSALAMUALAIKUM ☺️* \n *KESE HAI AP 🥰* \n *ALLAH AP SAB KO HAMESHA KHUSH RAKHE AMEEN 🤲💓*`;
+    const status = `*ASSALAMUALAIKUM ☺️* \n *KESE HAI AP 🥰* \n *UMEED HAI KE AP SAB KHARIYAT SE HOGE 😇 \n *ALLAH AP SAB KO HAMESHA KHUSH RAKHE AMEEN 🤲💓*`;
     await conn.sendMessage(from, { text: status });
   } catch (e) {
     console.error("*GGG.....🥺💓*", e);
