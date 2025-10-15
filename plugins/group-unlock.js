37253d463476fa8e1489d1abc6f0af443b005401
BILAL TECH <penalacc786@gmail.com>
2025-10-15 09:37:26 +0500

    👑 BILAL-MD 👑

--- plugins/group-unlock.js
+++ plugins/group-unlock.js
@@ -1,6 +1,6 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('../lib/functions')
 
 cmd({
     pattern: "unlockgc",
@@ -12,14 +12,32 @@ cmd({
 },           
 async (conn, mek, m, { from, isGroup, isAdmins, isBotAdmins, reply }) => {
     try {
-        if (!isGroup) return reply("*YEH COMMAND SIRF GROUPS ME USE KAREIN ☺️❤️*");
-        if (!isAdmins) return reply("*YEH COMMAND SIRF GROUP ADMINS USE KAR SAKTE HAI ☺️❤️*");
-        if (!isBotAdmins) return reply("*PEHLE MUJHE IS GROUP ME ADMIN BANAO ☺️❤️*");
+        // 🔹 Not a group
+        if (!isGroup) {
+            await conn.sendMessage(from, { react: { text: '😫', key: m.key } });
+            return reply("*YEH COMMAND SIRF GROUPS ME USE KAREIN ☺️❤️*");
+        }
 
+        // 🔹 Not an admin
+        if (!isAdmins) {
+            await conn.sendMessage(from, { react: { text: '🤐', key: m.key } });
+            return reply("*YEH COMMAND SIRF GROUP ADMINS USE KAR SAKTE HAI ☺️❤️*");
+        }
+
+        // 🔹 Bot is not admin
+        if (!isBotAdmins) {
+            await conn.sendMessage(from, { react: { text: '😎', key: m.key } });
+            return reply("*PEHLE MUJHE IS GROUP ME ADMIN BANAO ☺️❤️*");
+        }
+
+        // 🔹 Unlock group
         await conn.groupSettingUpdate(from, "unlocked");
-        reply("*AB YE GROUP UNLOCK HO CHUKA HAI 😊🌺* \n *AB IS GROUP ME NEW MEMBERS JOIN KAR SAKTE HAI ☺️🌹*");
+        await conn.sendMessage(from, { react: { text: '☺️', key: m.key } });
+        reply("*AB AP SAB MEMBERS IS GROUP KI PROFILE PIC AUR GROUP KA NAME ☺️ CHANGE YA EDIT KAR SAKTE HO 🥰🌹*");
+
     } catch (e) {
         console.error("*DUBARA KOSHISH KAREIN 🥺❤️*", e);
+        await conn.sendMessage(from, { react: { text: '😔', key: m.key } });
         reply("*DUBARA KOSHISH KAREIN 🥺❤️*");
     }
 });
