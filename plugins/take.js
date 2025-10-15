d3640d3558e88efe6383fb3e31658ad7381becfb
BILAL TECH <penalacc786@gmail.com>
2025-10-15 16:05:50 +0500

    👑 BILAL-MD 👑

--- plugins/take.js
+++ plugins/take.js
@@ -20,7 +20,7 @@ cmd(
     },
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*PEHLE KISI STICKER KO MENTION KARO 😊🌹*`);
-        if (!q) return reply(`*DUBARA KOSHISH KARO AUR 😊❤️* \n *.TAKE MADE BY APKA NAME* \n *JAB ESE LIKHO GE TO WO STICKER APKA NAME KA BAN JAYE GA 😊🌹❤️*`);
+        if (!q) return reply(`*DUBARA KOSHISH KARO AUR 😊❤️* \n *.TAKE MADE BY APKA NAME* \n *JAB ESE LIKHO GE TO WO STICKER APKA NAME KA BAN JAYE GA ☺️*`);
 
         let mime = mek.quoted.mtype;
         let pack = q;
@@ -44,37 +44,3 @@ cmd(
 );
 
 //Sticker create 
-
-cmd(
-    {
-        pattern: 'sticker',
-        alias: ['s', 'stickergif'],
-        desc: 'Create a sticker from an image, video, or URL.',
-        category: 'sticker',
-        use: '<reply media or URL>',
-        filename: __filename,
-    },
-    async (conn, mek, m, { quoted, args, q, reply, from }) => {
-        if (!mek.quoted) return reply(`*AP KISI PHOTO , VIDEO KO MENTION KARO AUR PHIR LIKHO ☺️🌹* \n STICKER* \n *JAB AP STICKER LIKHO GE TO APKI PHOTO YA VIDEO STICKER BAN JAYE GE OK 😊❤️*`);
-        let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "👑 BILAL-MD 👑";
-        
-        if (mime === "imageMessage" || mime === "stickerMessage") {
-            let media = await mek.quoted.download();
-            let sticker = new Sticker(media, {
-                pack: pack, 
-                type: StickerTypes.FULL,
-                categories: ["🤩", "🎉"], 
-                id: "12345",
-                quality: 75, 
-                background: 'transparent',
-            });
-            const buffer = await sticker.toBuffer();
-            return conn.sendMessage(mek.chat, { sticker: buffer }, { quoted: mek });
-        } else {
-            return reply("*ARE AP PHOTO YA VIDEO KO MENTION KARE BAS AUR KISI CHIZ KO NAI OK 😊❤️*");
-        }
-    }
-);
-
-// JawadTechX
