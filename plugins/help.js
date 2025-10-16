6597c9818ef057ddd50d05b1d538dade88c456d5
BILAL TECH <penalacc786@gmail.com>
2025-10-16 08:16:57 +0500

    👑 BILAL-MD 👑

--- plugins/help.js
+++ plugins/help.js
@@ -2,7 +2,7 @@ const { cmd } = require('../command');
 
 cmd({
     pattern: "help",
-    alias: ["menu", "commands"],
+    alias: ["hi", "error"],
     desc: "📜 Show bot help or command list.",
     react: "🥰",
     category: "main",
@@ -11,8 +11,9 @@ cmd({
 async (conn, mek, m, { from, reply }) => {
     try {
         await conn.sendMessage(from, { react: { text: '🥰', key: m.key } });
-        return reply("*https://akaserein.github.io/Bilal/*");
+        return reply("*👑 ClICK HERE FOR HELP 👑* \n\n*👑 SUPPORT WEBSITE 👑* \n*https://akaserein.github.io/Bilal/* \n\n *👑 SUPPORT CHANNEL 👑*  \n*https://whatsapp.com/channel/0029Vaj3Xnu17EmtDxTNnQ0G* \n\n*👑 SUPPORT GROUP 👑* \n*https://chat.whatsapp.com/BwWffeDwiqe6cjDDklYJ5m?mode=ems_copy_t* \n\n*👑 BILAL-MD WHATSAPP BOT 👑*
+");
     } catch (e) {
-        console.error("Help Command Error:", e);
+        console.error("*DUBARA LIKHO ❮HELP❯ 🥺*", e);
     }
 });
