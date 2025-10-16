17113fa92f04e8c5bd0081930b4ea8d3fe9c0959
BILAL TECH <penalacc786@gmail.com>
2025-10-16 08:18:17 +0500

    👑 BILAL-MD 👑

--- plugins/help.js
+++ plugins/help.js
@@ -11,8 +11,7 @@ cmd({
 async (conn, mek, m, { from, reply }) => {
     try {
         await conn.sendMessage(from, { react: { text: '🥰', key: m.key } });
-        return reply("*👑 ClICK HERE FOR HELP 👑* \n\n*👑 SUPPORT WEBSITE 👑* \n*https://akaserein.github.io/Bilal/* \n\n *👑 SUPPORT CHANNEL 👑*  \n*https://whatsapp.com/channel/0029Vaj3Xnu17EmtDxTNnQ0G* \n\n*👑 SUPPORT GROUP 👑* \n*https://chat.whatsapp.com/BwWffeDwiqe6cjDDklYJ5m?mode=ems_copy_t* \n\n*👑 BILAL-MD WHATSAPP BOT 👑*
-");
+        return reply("*👑 ClICK HERE FOR HELP 👑* \n\n*👑 SUPPORT WEBSITE 👑* \n*https://akaserein.github.io/Bilal/* \n\n *👑 SUPPORT CHANNEL 👑*  \n*https://whatsapp.com/channel/0029Vaj3Xnu17EmtDxTNnQ0G* \n\n*👑 SUPPORT GROUP 👑* \n*https://chat.whatsapp.com/BwWffeDwiqe6cjDDklYJ5m?mode=ems_copy_t* \n\n*👑 BILAL-MD WHATSAPP BOT 👑*");
     } catch (e) {
         console.error("*DUBARA LIKHO ❮HELP❯ 🥺*", e);
     }
